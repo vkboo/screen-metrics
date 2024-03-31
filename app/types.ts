348ec521279786926efbc6907f0cc19de7d6e193
zhaobo <vkbo@qq.com>
2024-03-31 23:31:00 +0800

    feat: temp

---
+++ app/types.ts
@@ -0,0 +1,11 @@
+type Item = {
+    id: string;
+    create_at: number;
+    update_at: number;
+    // email: string;
+    screen_size_auto_measure: string;
+    // screen_size_input: string;
+    // is_confirm_by_user: boolean;
+    country: string;
+    platform: object;
+}
